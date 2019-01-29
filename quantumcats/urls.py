543c0ba5b11eef62c08256df63d8f9eac34b6371
Christopher Lo <46541035+topher-lo@users.noreply.github.com>
2019-01-29 22:54:53 +0000

    initialise repo

---
+++ quantumcats/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+
+from game import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('catlapse/', views.catlapse, name='catlapse'),
+    path('entangle/', views.entangle, name='entangle'),
+    path('admin/', admin.site.urls),
+]
